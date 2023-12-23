f26bb1f94cf58784935f4c47546baa6d5f7e4edc
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-22 16:53:10 -0800

    fix(middleware): use middleware for all routes except /auth

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,10 @@
 const authMiddleware = require('./auth')
 function mountMiddlewares(app) {
-    app.use(authMiddleware)
+    app.use('/users', authMiddleware)
+    app.use('/projects', authMiddleware)
+    app.use('/events', authMiddleware)
+    app.use('/lists', authMiddleware)
+    app.use('/tasks', authMiddleware)
 }
 
 module.exports = mountMiddlewares
